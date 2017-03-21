097dc6f79a7bc23b349754e7e621467dc341540e
csi <ckthdlr@naver.com>
2017-03-21 16:18:05 +0900

    service merged

--- litStorage/src/service/logic/LiteratureServiceLogic.java
+++ litStorage/src/service/logic/LiteratureServiceLogic.java
@@ -15,94 +15,94 @@ import store.logic.LiteratureStoreLogic;
 
 public class LiteratureServiceLogic implements LiteratureService{
 	
-	private LiteratureStore store;
-	private EpisodeStore EpStore;
-	private ChangeHistoryStore ChStore;
+	private LiteratureStore lStore;
+	private EpisodeStore epStore;
+	private ChangeHistoryStore chStore;
 	
 	public LiteratureServiceLogic() {
-		store = new LiteratureStoreLogic();
-		EpStore = new EpisodeStoreLogic();
-		ChStore = new ChangeHistoryStoreLogic();
+		lStore = new LiteratureStoreLogic();
+		epStore = new EpisodeStoreLogic();
+		chStore = new ChangeHistoryStoreLogic();
 	}
 
 	@Override
 	public boolean registerLiterature(Literature literature) {
-		return store.insertLiterature(literature);
+		return lStore.insertLiterature(literature);
 	}
 
 	@Override
 	public boolean removeLiterature(String Id) {
-		return store.deleteLiterature(Id);
+		return lStore.deleteLiterature(Id);
 	}
 
 	@Override
 	public Literature findLiteratureById(String Id) {
-		return store.selectLiteraturesById(Id);
+		return lStore.selectLiteraturesById(Id);
 	}
 
 	@Override
 	public List<Literature> findLiteratureByName(String name) {
-		return store.selectLiteraturesByName(name);
+		return lStore.selectLiteraturesByName(name);
 	}
 
 	@Override
 	public List<Literature> findLiteratureByLitStorageId(String litStorageId) {
-		return store.selectLiteraturesByLitStorageId(litStorageId);
+		return lStore.selectLiteraturesByLitStorageId(litStorageId);
 	}
 
 	@Override
 	public List<Literature> findLiteratureByGenreOrderByHits() {
-		return store.selectLiteraturesByGenreOrderByHits();
+		return lStore.selectLiteraturesByGenreOrderByHits();
 	}
 
 	@Override
 	public List<Literature> findLiteratureByGenreOrderById(String Id) {
-		return store.selectLiteratureByGenreOrderById(Id);
+		return lStore.selectLiteratureByGenreOrderById(Id);
 	}
 
 	@Override
 	public boolean registerEpisode(Episode episode) {
-		return EpStore.insertEpisode(episode);
+		return epStore.insertEpisode(episode);
 	}
 
 	@Override
 	public boolean modifyEpisode(Episode episode) {
-		return EpStore.updateEpisode(episode);
+		return epStore.updateEpisode(episode);
 	}
 
 	@Override
 	public boolean removeEpisode(Episode episode) {
-		return EpStore.deleteEpisode(episode);
+		return epStore.deleteEpisode(episode);
 	}
 
 	@Override
 	public Episode findEpisodeById(String Id) {
-		return EpStore.selectEpisodeById(Id);
+		return epStore.selectEpisodeById(Id);
 	}
 
 	@Override
 	public List<Episode> findEpisodeByLiteratureId(String literatureId) {
-		return EpStore.selectEpisodesByLiteratureId(literatureId);
+		return epStore.selectEpisodesByLiteratureId(literatureId);
 	}
 
 	@Override
 	public boolean modifyBoundById(String Id) {
-		return EpStore.updateBound(Id);
+		return epStore.updateBound(Id);
 	}
 
 	@Override
 	public boolean registerChangeHistory(ChangeHistory changeHistory) {
-		return ChStore.insertChangeHistory(changeHistory);
+		return chStore.insertChangeHistory(changeHistory);
 	}
 
 	@Override
 	public List<ChangeHistory> findChangeHistoriesByEpisodeId(String episodeId) {
-		return ChStore.selectChangeHistoriesByEpisodeId(episodeId);
+		return chStore.selectChangeHistoriesByEpisodeId(episodeId);
 	}
 
 	@Override
 	public ChangeHistory findChangeHistoryById(String Id) {
-		return ChStore.selectChangeHistoryById(Id);
+		return chStore.selectChangeHistoryById(Id);
 	}
 
 }
--- litStorage/src/service/logic/MemberServiceLogic.java
+++ litStorage/src/service/logic/MemberServiceLogic.java
@@ -12,53 +12,53 @@ import store.logic.MemberStoreLogic;
 
 public class MemberServiceLogic implements MemberService{
 
-	private MemberStore MStore;
-	private InviteRequestStore IStore;
+	private MemberStore mStore;
+	private InviteRequestStore iStore;
 	
 	public MemberServiceLogic() {
-		MStore=new MemberStoreLogic();
-		IStore=new InviteRequestStoreLogic();
+		mStore=new MemberStoreLogic();
+		iStore=new InviteRequestStoreLogic();
 	}
 	
 	
 	@Override
 	public boolean registerMember(Member member) {
-		return MStore.insertMember(member);
+		return mStore.insertMember(member);
 	}
 
 	@Override
 	public Member findMemberById(String id) {
-		return MStore.selectMemberById(id);
+		return mStore.selectMemberById(id);
 	}
 
 	@Override
 	public boolean modifyMember(Member member) {
-		return MStore.updateMember(member);
+		return mStore.updateMember(member);
 	}
 
 	@Override
 	public boolean removeMember(String id) {
-		return MStore.deleteMember(id);
+		return mStore.deleteMember(id);
 	}
 
 	@Override
 	public boolean registerInviteRequest(InviteRequest inviteRequest) {
-		return IStore.insertInviteRequest(inviteRequest);
+		return iStore.insertInviteRequest(inviteRequest);
 	}
 
 	@Override
 	public boolean removeInviteRequest(String senderId, String receiverId) {
-		return IStore.deleteInviteRequest(senderId, receiverId);
+		return iStore.deleteInviteRequest(senderId, receiverId);
 	}
 
 	@Override
 	public List<InviteRequest> findInviteRequestsBySenderId(String senderId) {
-		return IStore.selectInviteRequestBySenderId(senderId);
+		return iStore.selectInviteRequestBySenderId(senderId);
 	}
 
 	@Override
 	public List<InviteRequest> findInviteRequestsByReceiverId(String receiverId) {
-		return IStore.selectInviteRequestByReceiverId(receiverId);
+		return iStore.selectInviteRequestByReceiverId(receiverId);
 	}
 
 }
