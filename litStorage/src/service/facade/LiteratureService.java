13c3f6fd894892440cfbdf3c019b3717c95bb466
eomjaeun <flutter.redien@gmail.com>
2017-03-21 15:53:24 +0900

    happy happy

---
+++ litStorage/src/service/facade/LiteratureService.java
@@ -0,0 +1,42 @@
+package service.facade;
+
+import java.util.List;
+
+import domain.ChangeHistory;
+import domain.Episode;
+import domain.Literature;
+
+public interface LiteratureService {
+	public boolean registerLiterature(Literature literature);
+
+	public boolean removeLiterature(String Id);
+
+	public Literature findLiteratureById(String Id);
+
+	public List<Literature> findLiteratureByName(String name);
+
+	public List<Literature> findLiteratureByLitStorageId(String litStorageId);
+
+	public List<Literature> findLiteratureByGenreOrderByHits();
+
+	public List<Literature> findLiteratureByGenreOrderById(String Id);
+
+	public boolean registerEpisode(Episode episode);
+
+	public boolean modifyEpisode(Episode episode);
+
+	public boolean removeEpisode(Episode episode);
+
+	public Episode findEpisodeById(String Id);
+
+	public List<Episode> findEpisodeByLiteratureId(String literatureId);
+
+	public boolean modifyBoundById(String Id);
+
+	public boolean registerChangeHistory(ChangeHistory changeHistory);
+
+	public List<ChangeHistory> findChangeHistoriesByEpisodeId(String episodeId);
+
+	public ChangeHistory findChangeHistoryById(String Id);
+
+}
--- litStorage/src/service/facade/MemberService.java
+++ litStorage/src/service/facade/MemberService.java
@@ -1,4 +1,4 @@
-package service;
+package service.facade;
 
 public interface MemberService {
 
