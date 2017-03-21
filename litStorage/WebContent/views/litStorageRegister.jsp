0293daeeb5c33b18925727de6922172bf6cd14b6
csi <ckthdlr@naver.com>
2017-03-21 17:53:21 +0900

    litStorageAllList.jsp and Controller tested, litStorageRegister.jsp and Controller tested

--- litStorage/WebContent/views/litStorageRegister.jsp
+++ litStorage/WebContent/views/litStorageRegister.jsp
@@ -1,12 +1,29 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-	
-</body>
+<c:set var="ctx">${pageContext.request.contextPath }</c:set>
+<html lang="ko">
+	<head>
+	<title>작품 저장소 등록</title>
+	<meta charset="utf-8">
+	<link href="resources/css/bootstrap_modify.css" rel="stylesheet">
+	<link href="resources/css/layout.css" rel="stylesheet">
+	<script src="resources/js/jquery-2.1.3.js"></script>
+	</head>
+	<body>
+		<form action="${ctx }/litStorage/register.do" method="post">
+			<table>
+				<tr>
+					<td>이름</td>
+					<td><input type="text" name="name" maxlength="30"></td>
+				</tr>
+				<tr>
+					<td>소개</td>
+					<td><input type="text" name="introduce" maxlength="300"></td>
+				</tr>
+			</table>
+			<button type="submit">등록</button>
+		</form>
+	</body>
 </html>
--- litStorage/src/controller/LitStorageRegisterController.java
+++ litStorage/src/controller/LitStorageRegisterController.java
@@ -7,14 +7,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/litstorage/register.do")
+import domain.LitStorage;
+import domain.Member;
+import service.facade.LitStorageService;
+import service.logic.LitStorageServiceLogic;
+
+@WebServlet("/litStorage/register.do")
 public class LitStorageRegisterController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LitStorageService service = new LitStorageServiceLogic();
+		
+		String name = request.getParameter("name");
+		String introduce = request.getParameter("introduce");
+		
+		if(name == null || name.isEmpty()) {
+			throw new RuntimeException("작품저장소의 이름을 입력해주세요");	// validate() 필요
+		}
+		
+		LitStorage litStorage = new LitStorage();
+		
+		Member creator = new Member();
+//		creator.setId((String)request.getSession().getAttribute("userId"));	// 세선에 넣을 Attribute이름을 정해야 함
+		creator.setId("dd");	// 테스트용 임시ID
+		
+		litStorage.setCreator(creator);
+		litStorage.setName(name);
+		litStorage.setIntroduce(introduce);
+		
+		if(!service.registerLitStorage(litStorage)) {
+			throw new RuntimeException("작품저장소 생성 실패");
+		}
+		
+		response.sendRedirect(request.getContextPath() + "/views/litStorageMyStorageList.jsp");
 	}
 
 }
