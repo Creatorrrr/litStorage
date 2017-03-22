068c73e349c1823c644cf37c60f8eea58f0f7d31
Maju <akfoddl@gmail.com>
2017-03-22 17:12:43 +0900

    try to make memberSearchAJAXController but time is over...

--- litStorage/WebContent/views/joinedMemberList.jsp
+++ litStorage/WebContent/views/joinedMemberList.jsp
@@ -6,7 +6,12 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link href="/resources/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
 <title>Insert title here</title>
 </head>
 <body>
--- litStorage/src/controller/LitStorageProfileController.java
+++ litStorage/src/controller/LitStorageProfileController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import domain.LitStorage;
+import domain.Member;
 import service.facade.LitStorageService;
 import service.logic.LitStorageServiceLogic;
 
@@ -22,8 +23,27 @@ public class LitStorageProfileController extends HttpServlet {
 		String id = request.getParameter("id");
 		LitStorageService service = new LitStorageServiceLogic();
 		
+		//add LitStorage to request and send to jsp
 		LitStorage ls = service.findLitStorageById(id);
+		
+		String loginId = (String)request.getSession().getAttribute("loginId");
+		
+		/*search whether member already join that LitStorage or not.
+		 * if already joined or member is creator, flag will set false.
+		 * or not, flag will set true. if true is set to request, join request
+		 * button will be appear on litStorageProfile.jsp*/
+		boolean flag = true;
+		if(!loginId.equals(ls.getCreator().getId())){
+			for(Member m : ls.getParticipants()){
+				if(m.getId().equals(loginId)){
+					flag = false;
+				}
+			}
+		}else{
+			flag = false;
+		}
 		request.setAttribute("litStorage", ls);
+		request.setAttribute("isNotJoined", flag);
 		request.getRequestDispatcher("/views/litStorageProfile.jsp").forward(request, response);
 	}
 
