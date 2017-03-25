df05bcb17739a5042abd0b1a6cf566d58da2524c
csi <ckthdlr@naver.com>
2017-03-25 17:56:25 +0900

    0325 finished

--- litStorage/src/constants/Constants.java
+++ litStorage/src/constants/Constants.java
@@ -1,8 +1,8 @@
 package constants;
 
 public class Constants {
-	public static final String ROOT_PATH = "C:/Users/\uC774\uC911\uC624/litStorageGit";
-	public static final String IMAGE_PATH = "C:/Users/\uC774\uC911\uC624/litStorageImgs";
+	public static final String ROOT_PATH = "C:/Users/kosta/litStorageGit";
+	public static final String IMAGE_PATH = "C:/Users/kosta/litStorageImgs";
 	
 	public static final String BOUND_MEMBER = "M";
 	public static final String BOUND_ALL = "A";
