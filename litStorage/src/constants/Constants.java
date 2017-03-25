c828d5f6539ba9dcc8c93fdd153e09071a5f025b
Maju <akfoddl@gmail.com>
2017-03-25 23:38:59 +0900

    make dataGenerator

--- litStorage/src/constants/Constants.java
+++ litStorage/src/constants/Constants.java
@@ -1,6 +1,8 @@
 package constants;
 
 public class Constants {
-	public static final String ROOT_PATH = "C:/Users/kosta/litStorageGit";
+//	public static final String ROOT_PATH = "C:/Users/kosta/litStorageGit";
+	//마주환용 git path 지우지 마세요ㅠ
+	public static final String ROOT_PATH = "C:/Users/ma ju hwan/Documents/litStorageGit";
 	public static final String BOUND_MEMBER = "M";
 }
---
+++ litStorage/test/dataTest/DataGenerator.java
@@ -0,0 +1,148 @@
+package dataTest;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+
+import domain.Board;
+import domain.LitStorage;
+import domain.Literature;
+import domain.Member;
+import service.facade.LitStorageService;
+import service.facade.LiteratureService;
+import service.facade.MemberService;
+import service.logic.BoardServiceLogic;
+import service.logic.LitStorageServiceLogic;
+import service.logic.LiteratureServiceLogic;
+import service.logic.MemberServiceLogic;
+
+public class DataGenerator {
+
+	private LitStorageService lsService;
+	private LiteratureService lService;
+	private MemberService mService;
+	private List<Member> memberList;
+	private List<String> memberIdList;
+
+	public DataGenerator() {
+		lsService = new LitStorageServiceLogic();
+		lService = new LiteratureServiceLogic();
+		mService = new MemberServiceLogic();
+		memberIdList = new ArrayList<>();
+		memberList = mService.findMemberByName("");
+		for (Member m : memberList) {
+			memberIdList.add(m.getId());
+		}
+	}
+
+	// @Test
+	// public void makeLitStorage() {
+	// List<String> nameList = new ArrayList<>();
+	// try {
+	// BufferedReader in = new BufferedReader(new
+	// FileReader("litStorageName.txt"));
+	// String str;
+	//
+	// while ((str = in.readLine()) != null) {
+	// nameList.add(str);
+	// }
+	//
+	// int nameRand;
+	// int introRand;
+	// int writerRand;
+	// for(int i = 0 ; i < nameList.size(); i++){
+	// nameRand = (int)(Math.random() * (nameList.size()));
+	// introRand = (int)(Math.random() * (nameList.size()));
+	// writerRand = (int)(Math.random() * (nameList.size()));
+	// LitStorage lit = new LitStorage();
+	// lit.setName(nameList.get(nameRand));
+	// lit.setIntroduce(nameList.get(introRand));
+	// lit.setCreator(memberList.get(writerRand));
+	// lsService.registerLitStorage(lit);
+	// }
+	//
+	//
+	// in.close();
+	// } catch (FileNotFoundException e) {
+	// e.printStackTrace();
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	//
+	//
+	// assertEquals(true,true);
+	// }
+
+	@Test
+	public void makeLiterature() {
+		Document doc = null;
+		Elements titleContents = null;
+		Elements imageContents = null;
+		List<LitStorage> lsList = lsService.findAll();
+		List<Board> genreList = new BoardServiceLogic().findAllBoards();
+		List<String> introList = new ArrayList<>();
+		
+		int lsRand, genreRand, introRand, memberRand;
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("litStorageName.txt"));
+			 String str;
+				
+			 while ((str = in.readLine()) != null) {
+				 introList.add(str);
+			 }
+			 in.close();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// liter.setName();
+		// liter.setGenre();
+		// liter.setImagePath();
+		// liter.setIntroduce();
+		// liter.setCreator();
+		// liter.setHits();
+		// liter.setLitStorage();
+		
+		for (int i = 1; i <= 200; i++) {
+			try {
+				doc = Jsoup.connect("https://ridibooks.com/category/books/1710?page="+i).get();
+				titleContents = doc.select("h3.meta_title span.title_text");
+				imageContents = doc.select("div.thumbnail_image img");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			for (int j = 0; j < 20; j++) {
+				lsRand = (int)(Math.random() * (300)+1);
+				genreRand = (int)((Math.random()*7)-1);
+				introRand = (int)(Math.random() * (introList.size()));
+				memberRand = (int)(Math.random() * (memberList.size()));
+				Literature liter = new Literature();
+				liter.setName(titleContents.get(j).text());
+				liter.setGenre(genreList.get(genreRand).getTitle());
+				liter.setImagePath(imageContents.get(j).attr("src"));
+				liter.setIntroduce(introList.get(introRand));
+				liter.setCreator(memberList.get(memberRand));
+				liter.setHits((int)(Math.random()*90000));
+				liter.setLitStorage(lsList.get(lsRand));
+				
+				lService.registerLiterature(liter);
+			}
+		}
+		
+		
+		assertEquals(true, true);
+	}
+}
